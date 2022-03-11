4b1677dd16fd9923b663018e8963b517d990f9d1
Naman-Saxena1 <namansaxena.official@gmail.com>
2022-03-11 23:48:45 +0530

    fix: Removed index css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { ProductsProvider } from "./Context/product-context"
 import { GenreProvider } from "./Context/genre-context"
