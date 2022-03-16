443dbf31cc23c58e468aa62e2edacf37331d5c55
Naman-Saxena1 <namansaxena.official@gmail.com>
2022-03-16 22:32:49 +0530

    feat: Add basic backend calls to the server

--- src/index.js
+++ src/index.js
@@ -3,21 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ProductsProvider } from "./Context/product-context"
 import { GenreProvider } from "./Context/genre-context"
+import { NewArrivalsProvider } from "./Context/new-arrival-context"
 
 export { Navbar } from "./Components/Navbar/Navbar"
 export { GenreCard } from './Components/GenreCards/GenreCard'
-export { newArrivalsProductList } from "./Components/NewArrivals/new-arrivals"
+export { NewArrivals } from "./Components/NewArrivals/NewArrival"
 export { ProductCard } from "./Components/Card/ProductCard"
 export { Footer } from "./Components/Footer/Footer"
 export { Sidebar } from './Components/Sidebar/Sidebar'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GenreProvider>
-      <ProductsProvider>
-        <App/>
-      </ProductsProvider>
-    </GenreProvider>
+    <NewArrivalsProvider>
+      <GenreProvider>
+        <ProductsProvider>
+          <App/>
+        </ProductsProvider>
+      </GenreProvider>
+    </NewArrivalsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
